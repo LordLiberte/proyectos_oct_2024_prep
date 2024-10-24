f0ea1cc94cbdbf7d8d36287e9d5e88c14254aba9
Lord Liberté <GhostRg110@gmail.com>
2024-10-24 19:49:12 +0200

    Practica de un semaforo

--- sesion02/05_El_Semaforo__estructura_control.py
+++ sesion02/05_El_Semaforo__estructura_control.py
@@ -1,6 +1,21 @@
 # Semaforo
-verde = True
+verde = 1
+amarillo = 2
+rojo = 3
 
-if verde:
+semaforo = 1
+
+if semaforo == verde:
     # Entramos en el bucle if
-    print("esta en verde, puedes pasar")
+    print("Esta en verde")
+    print("Puedes pasar")
+    print("He pasado")
+
+elif semaforo == amarillo:
+    print("Esta en amarillo")
+    print("Ojo!!!!")
+    print("He pasado")
+
+else:
+    print("Esta en rojo")
+    print("No puedes pasar")
