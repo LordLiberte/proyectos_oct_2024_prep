558abb523ba7760d8a43fd402fe3a5fa95bb9e42
Lord Liberté <GhostRg110@gmail.com>
2024-10-24 19:19:58 +0200

    Sesion 02. Fundamentos de Programacion.
    Se crean archivos para comenzar a trabajar con variables y sus tipos de datos.

---
+++ sesion02/sesion02_variables01.py
@@ -0,0 +1,17 @@
+# Definición de variables
+
+canastas = 7
+canastas_paco = 2
+canastas_pau = 8
+canastas_eloi = 7
+ptos_tres = 3
+
+# Calculos
+total_puntos_paco = canastas_paco * ptos_tres
+total_puntos_pau = canastas_pau * ptos_tres
+total_puntos_eloi = canastas_eloi * ptos_tres
+
+# Ver puntuaciones
+print(total_puntos_eloi)
+print(total_puntos_paco)
+print(total_puntos_pau)
