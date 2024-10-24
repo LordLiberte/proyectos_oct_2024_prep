2b522f7f1249c907cbe66686f930ce9fbcf883f0
Lord Liberté <GhostRg110@gmail.com>
2024-10-24 19:55:19 +0200

    Update 06_Cadenas_texto.py

---
+++ sesion02/06_Cadenas_texto.py
@@ -0,0 +1,9 @@
+"""Esto es un comentario"""
+# Print
+print("Hola Pakito")
+
+# Diferentes definiciones
+
+cadena_simple = 'Hola Mundo'
+cadena_doble = "Hola mundo"
+cadena_triple = """Este es un ejemplo"""
