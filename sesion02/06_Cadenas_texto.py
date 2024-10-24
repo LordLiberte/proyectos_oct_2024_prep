a7828f52d35f366d8d70db53d1e2cf11e1f78eb0
Lord Liberté <GhostRg110@gmail.com>
2024-10-24 19:57:59 +0200

    Update 06_Cadenas_texto.py

--- sesion02/06_Cadenas_texto.py
+++ sesion02/06_Cadenas_texto.py
@@ -7,3 +7,14 @@ print("Hola Pakito")
 cadena_simple = 'Hola Mundo'
 cadena_doble = "Hola mundo"
 cadena_triple = """Este es un ejemplo"""
+
+# Concatenación
+saludo = "Hello"
+nombre = "Pakito"
+
+print(saludo+" "+nombre)
+
+# F-string
+nombre = "Carlos"
+
+print(f"Me llamo {nombre}, un placer")
