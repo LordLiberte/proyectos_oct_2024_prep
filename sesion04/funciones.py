eae8154dd895d286feb7ab214554b0a653e9c76b
Lord Liberté <GhostRg110@gmail.com>
2024-11-14 18:14:49 +0100

    Create funciones.py

---
+++ sesion04/funciones.py
@@ -0,0 +1,9 @@
+# La principal ventaja de las funciones es su reusabilidad.
+# Podemos llamarla siempre que queramos psandole ciertos parametros.
+# Otra ventaja es su modularidad. Consiste en crear un codigo basado en funciones que se llamen unos a otros
+# Haciendo que el codigo sea más denso pero más legible y escalable
+
+def saludo():
+    print("Hola Mundo")
+
+saludo()
