f8432309e5de3e1407a56025a068619650b3b445
Lord Liberté <GhostRg110@gmail.com>
2024-11-20 19:06:57 +0100

    Update Gestion_Jugadores.py

--- Portfolio/Actividad_1/Gestion_Jugadores.py
+++ Portfolio/Actividad_1/Gestion_Jugadores.py
@@ -53,25 +53,55 @@ def saludo():
     decision = input(f"¿Desea iniciar sesión, {nombre}? [Si/No]\n").lower()
     return decision
 
+# ===================================================================
+# OPCIÓN 1. Introducir nuevo jugador
+# ===================================================================
+def nuevo_jugador(nombre, dorsal, Canastas_3, Canastas_2, Canastas_1):
+    lista_juadores = []
+    jugador = {"Nombre": nombre, "Dorsal": dorsal, "Canastas de 3": Canastas_3,
+               "Canastas de 2": Canastas_2, "Canatas de 1": Canastas_1}
+    lista_juadores.append(jugador)
+    return lista_juadores
 
 # CICLO DE PROGRAMA -----------------------------------------------------------------------
 
 decision = saludo()  # guarda la decisión del usuario
-while True:
-    if decision == "si":  # condición del usuario si acepta o no iniciar sesión
 
+while True:
+
+    if decision == "no":  # decisión "No" del usuario
+        print("Una pena... Nos vemos en la próxima")
+        break
+
+    elif decision == "si":  # condición del usuario si acepta o no iniciar sesión
+
+        # ---------------------------------------------------------------------
+        # Bloque validación entrada de usuario
         try:
             opcion_elegida = int(menu())  # guarda la opción elegida del usuario
 
+        # Para valores incorrectos
         except ValueError:
             print("Debe ser un número, vuelva a intentarlo")
             time.sleep(2)
             pass
-
+        # Para valores correctos
         else:
             if opcion_elegida == 0:
                 print("¡Espero verte pronto!")
                 time.sleep(2)
                 break
 
+            if opcion_elegida == 1:
+                nombre = input("Nombre del jugador: ")
+                dorsal = input("Dorsal del jugador: ")
+                Canastas_3 = input("Número de canastas de 3: ")
+                Canastas_2 = input("Número de canastas de 2: ")
+                Canastas_1 = input("Número de canastas de 1: ")
+                jugadores_listados = nuevo_jugador(nombre, dorsal, Canastas_3,
+                                                  Canastas_2, Canastas_1)
+                print("¡Jugador creado!")
+                time.sleep(0.5)
+
+        # ---------------------------------------------------------------------
 
