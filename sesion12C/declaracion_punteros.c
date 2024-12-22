f42c34e6f5a7154b1e27d6d9075f7ab30f02a33d
Lord Liberté <GhostRg110@gmail.com>
2024-12-22 06:33:55 +0100

    a

--- sesion12C/declaracion_punteros.c
+++ sesion12C/declaracion_punteros.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int *puntero;
+int *ptr1;
+int *ptr2;
 
 int main(){
     int  a=10;  // variable integer
@@ -8,5 +10,11 @@ int main(){
     printf("El valor de a es: %d\n", a);
     printf("La dirección de a es: %p\n", puntero);
 
+    int mi_array[] = {1, 2, 3, 4, 5};  // Un array debe llevar []
+    ptr1 = &mi_array[0]; // Ahora el ptr apunta al primer entero de nuestro array
+    ptr2 = &mi_array[1]; // Ahora el ptr apunta al segundo entero de nuestro array
+
+    printf("El valor de mi_array[0] es: %d\n", ptr1);
+    printf("El valor de mi_array[1] es: %d\n", ptr2);
     return 0; // Sirve para indicar que el programa terminó correctamente
 }
